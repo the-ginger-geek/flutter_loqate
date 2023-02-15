cf5475062d294be88c6b7d4b2ca0bcd3a0e41aaf
Tapiwa Tererai <tapiwatererai@outlook.com>
2023-02-15 12:17:04 +0200

    remove io client and replaced with http_client

--- lib/infrastructure/network/repository.dart
+++ lib/infrastructure/network/repository.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:http/io_client.dart';
 import '../../constants/api_constants.dart';
 import '../../domain/address.dart';
 import '../../domain/retrieve_response.dart';
@@ -9,25 +6,32 @@ import '../../domain/find_item.dart';
 import '../../domain/find_request.dart';
 import '../../domain/find_response.dart';
 import 'http_client.dart';
+import 'package:http/http.dart' as http;
 
 class Repository {
   final String apiKey;
 
   Repository(this.apiKey);
 
-  Client get _httpClient => Client(baseUrl: ApiConstants.baseUrl, httpClient: IOClient(HttpClient()));
+  Client get _httpClient =>
+      Client(baseUrl: ApiConstants.baseUrl, httpClient: http.Client());
 
   Future<List<FindItem>> findAddresses(FindRequest searchRequest) async {
-    return FindResponse.fromJson(await _httpClient.get(
-      endpoint: ApiConstants.find,
-      queryParameters: searchRequest.toJson()..addAll({'Key': apiKey}),
-    )).searchItems;
+    return FindResponse.fromJson(
+      await _httpClient.get(
+        endpoint: ApiConstants.find,
+        queryParameters: searchRequest.toJson()..addAll({'Key': apiKey}),
+      ),
+    ).searchItems;
   }
 
-  Future<List<Address>> retrieveAddresses(RetrieveRequest retrieveRequest) async {
-    return RetrieveResponse.fromJson(await _httpClient.get(
-      endpoint: ApiConstants.retrieve,
-      queryParameters: retrieveRequest.toJson()..addAll({'Key': apiKey}),
-    )).addresses;
+  Future<List<Address>> retrieveAddresses(
+      RetrieveRequest retrieveRequest) async {
+    return RetrieveResponse.fromJson(
+      await _httpClient.get(
+        endpoint: ApiConstants.retrieve,
+        queryParameters: retrieveRequest.toJson()..addAll({'Key': apiKey}),
+      ),
+    ).addresses;
   }
 }
